d8eaca284d73701e30bda6bc966366b7ddba960b
Emilie Sing <sing.emilie.es@gmail.com>
2022-11-11 15:08:51 -0500

    Completed 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,7 +8,7 @@ const Button = (props) => (
   </button>
 )
 
-const Statistics = (props) => <p>{props.text} {props.value}</p>
+const Statistics = (props) => <p>{props.text} {props.value} {props.sign}</p>
 
 const App = () => {
   // save clicks of each button to its own state
@@ -16,6 +16,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad 
+  
+  let positive = 0
+  let average = 0
+
+  if (all !== 0){
+    average = (good * 1 + bad * -1) / all
+    positive = (good / all) * 100
+  }
+
+  // put percentage sign for positive
   return (
     <div>
       <Title value='give feedback' />
@@ -26,6 +37,10 @@ const App = () => {
       <Statistics text='good' value={good} />
       <Statistics text='neutral' value={neutral} />
       <Statistics text='bad' value={bad} />
+      <Statistics text='all' value={all} />
+      <Statistics text='average' value={average} />
+      <Statistics text='positive' value={positive} sign='%'/> 
+
     </div>
   )
 }
