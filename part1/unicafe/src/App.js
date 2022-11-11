5f29c83670b7e12b0b5ad777074cd273a3ba00b1
Emilie Sing <sing.emilie.es@gmail.com>
2022-11-11 13:20:43 -0500

    Completed 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,15 @@
 import { useState } from 'react'
 
+const Title = props => <h1>{props.value}</h1>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistics = (props) => <p>{props.text} {props.value}</p>
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +18,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Title value='give feedback' />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Title value='statistics' />
+      <Statistics text='good' value={good} />
+      <Statistics text='neutral' value={neutral} />
+      <Statistics text='bad' value={bad} />
     </div>
   )
 }
