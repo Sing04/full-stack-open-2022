641bd5f3c55e9da230b4223076a124d7ecd02525
Emilie Sing <sing.emilie.es@gmail.com>
2022-11-11 15:22:04 -0500

    Completed 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -17,32 +17,37 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const all = good + neutral + bad 
-  
-  let positive = 0
-  let average = 0
 
   if (all !== 0){
-    average = (good * 1 + bad * -1) / all
-    positive = (good / all) * 100
+    const average = (good * 1 + bad * -1) / all
+    const positive = (good / all) * 100
+    return(
+      <div>
+        <Title value='give feedback' />
+        <Button handleClick={() => setGood(good + 1)} text='good' />
+        <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button handleClick={() => setBad(bad + 1)} text='bad' />
+        <Title value='statistics' />
+        <Statistics text='good' value={good} />
+        <Statistics text='neutral' value={neutral} />
+        <Statistics text='bad' value={bad} />
+        <Statistics text='all' value={all} />
+        <Statistics text='average' value={average} />
+        <Statistics text='positive' value={positive} sign='%'/> 
+      </div>
+    )
+  } else{
+    return(
+      <div>
+        <Title value='give feedback' />
+        <Button handleClick={() => setGood(good + 1)} text='good' />
+        <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button handleClick={() => setBad(bad + 1)} text='bad' />
+        <Title value='statistics' />
+        <p>No feedback given</p>
+      </div>
+    )  
   }
-
-  // put percentage sign for positive
-  return (
-    <div>
-      <Title value='give feedback' />
-      <Button handleClick={() => setGood(good + 1)} text='good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
-      <Title value='statistics' />
-      <Statistics text='good' value={good} />
-      <Statistics text='neutral' value={neutral} />
-      <Statistics text='bad' value={bad} />
-      <Statistics text='all' value={all} />
-      <Statistics text='average' value={average} />
-      <Statistics text='positive' value={positive} sign='%'/> 
-
-    </div>
-  )
 }
 
 export default App
