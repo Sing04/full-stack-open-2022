05b4b98fe7825f754480b0f400a352f63a724e69
Emilie Sing <sing.emilie.es@gmail.com>
2022-11-15 16:26:25 -0500

    Completed 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,11 +4,17 @@ const Person = (person) => (<p>{person.name} {person.number}</p>)
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '514-123-4567' }
+    { name: 'Arto Hellas', number: '040-123456'},
+    { name: 'Ada Lovelace', number: '39-44-5323523'},
+    { name: 'Dan Abramov', number: '12-43-234345'},
+    { name: 'Mary Poppendieck', number: '39-23-6423122'}
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setnewSearch] = useState('')
+  const [searchedNames, setNewSearchedNames] = useState([])
+
+  const names = persons.map(person => person.name)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -18,10 +24,18 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleNameSearchChange = (event) => {
+    setnewSearch(event.target.value)
+    console.log(event.target.value)
+
+    const searchedNamesList = event.target.value !== '' ? persons.filter(person => person.name.includes(event.target.value)) : []
+
+    setNewSearchedNames(searchedNamesList)
+  }
+
   const addPerson = (event) => {
 
     event.preventDefault()
-    const names = persons.map(person => person.name)
     
     if (names.includes(newName)){
       window.alert(`${newName} is already added to phonebook`)
@@ -40,6 +54,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div> filter shown with <input value={newSearch} onChange={handleNameSearchChange}/></div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div> name: <input value={newName} onChange={handleNameChange}/></div>
         <div> number: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -48,7 +64,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {searchedNames.map(person =>
         <Person key={person.name} name={person.name} number={person.number}/>)}
     </div>
   )
