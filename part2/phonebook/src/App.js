db27c3331e0c1ee0410979d81e464765f2fd0676
Emilie Sing <sing.emilie.es@gmail.com>
2022-11-16 14:54:32 -0500

    Installed json server globally and completed 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,38 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const Person = (person) => (<p>{person.name} {person.number}</p>)
+const Persons = ({persons}) => {
+  return (
+    <div>
+      {persons.map(person => <Person key={person.name} person={person}/>)}
+    </div>
+  )
+}
+
+const Person = ({person}) => (<p>{person.name} {person.number}</p>)
+
+const Filter = (props) => {
+  return (
+    <div> filter shown with <input value={props.value} onChange={props.function}/></div>
+  )
+}
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456'},
-    { name: 'Ada Lovelace', number: '39-44-5323523'},
-    { name: 'Dan Abramov', number: '12-43-234345'},
-    { name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setnewSearch] = useState('')
   const [searchedNames, setNewSearchedNames] = useState([])
 
   const names = persons.map(person => person.name)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -26,7 +44,6 @@ const App = () => {
 
   const handleNameSearchChange = (event) => {
     setnewSearch(event.target.value)
-    console.log(event.target.value)
 
     const searchedNamesList = event.target.value !== '' ? persons.filter(person => person.name.includes(event.target.value)) : []
 
@@ -54,8 +71,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div> filter shown with <input value={newSearch} onChange={handleNameSearchChange}/></div>
-      <h2>Add a new</h2>
+      <Filter value={newSearch} function={handleNameSearchChange} />
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div> name: <input value={newName} onChange={handleNameChange}/></div>
         <div> number: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -63,9 +80,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {searchedNames.map(person =>
-        <Person key={person.name} name={person.name} number={person.number}/>)}
+      <h3>Numbers</h3>
+      <Persons persons={searchedNames} />
     </div>
   )
 }
