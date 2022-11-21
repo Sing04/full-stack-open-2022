8db77c2b9796f37ce95a3088c543ed9758f5de75
Emilie Sing <sing.emilie.es@gmail.com>
2022-11-21 14:42:05 -0500

    Completed 2.14

--- part2/countriesdata/src/App.js
+++ part2/countriesdata/src/App.js
@@ -5,79 +5,103 @@ import axios from 'axios'
 const Search = ({countries}) => {
   const [search, setSearch] = useState('')
   const [searchedCountries, setSearchedCountries] = useState([])
-
-
+  const [isShown, setIsShown] = useState(false)
 
   const handleSearchCountries = (event) => {
       setSearch(event.target.value)
 
-      //Ensure that search is not capital sensitive, seems to lower case the country names    //SORT NAMES ALPHABETICALLY
       const searchedCountriesList = event.target.value !== '' ? countries.filter(country => country.name.common.toLowerCase().includes(event.target.value.toLowerCase())) : []
       setSearchedCountries(searchedCountriesList)
+      setIsShown(false)
   }
 
  return (
     <div> 
       <div>find countries: <input value={search} onChange={handleSearchCountries}/></div>
-      <CountriesList countries={searchedCountries} />
+      <CountriesList countries={searchedCountries} isShown={isShown} setIsShown={setIsShown}/>
     </div>
-
   )
 }
 
-const CountriesList = ({countries}) => {
+const CountriesList = (props) => {
+  const countries = props.countries
 
-  const countryData = CountriesDisplay(countries)
+  const [clickedCountry, setClickedCountry] = useState()
 
+  const [countryData, singleCountryData] = CountriesDisplay(countries)
 
-   //Make sure to display one, 10 or text
+  function handleClick(country){
+
+    props.setIsShown(true)
+    setClickedCountry(country)
+  }
+
   function CountriesDisplay(searchedCountries) {
     
-      const countriesLength = searchedCountries.length
-      let countryInfo;
+    const countriesLength = searchedCountries.length
 
-      if (countriesLength > 10){
-        
-        countryInfo = <div>Too many matches, specify another filter</div>
-        
-      } else if (countriesLength <= 10 && countriesLength > 1){
-        const sortedCountriesList = countries.map(country => country.name.common).sort()
+    let countryInfo;
+    let singleCountryInfo;
 
-        countryInfo = <ul> {sortedCountriesList.map(country => <li key={country}>{country}</li>)} </ul>
-      } else if (countriesLength === 1){
-          const country = searchedCountries[0]
+    if (countriesLength > 10){
+      
+      countryInfo = <div>Too many matches, specify another filter</div>
+      
+    } else if (countriesLength <= 10 && countriesLength > 1){
 
-          countryInfo = <div>
-            <h1>{country.name.common}</h1>
-            <div>
-              <p>Capital: {country.capital}</p>
-              <p>Area: {country.area}</p>
-            </div>
-            <div>
-              <strong>Languages:</strong>
-              <ul>
-                {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
-              </ul>
-            </div>
-            <div>
-              <img src={country.flags.png} alt='country flag'/>
-            </div>
-          </div>
+      const sortedCountriesList = countries.sort(function (a, b) {
+        if (a.name.common < b.name.common){
+          return -1
+        } else if (a.name.common > b.name.common){
+          return 1
+        } else{
+          return 0
+        }
+      })
+
+      countryInfo = <ul> {sortedCountriesList.map(country => <li key={country.name.common}>{country.name.common} <button onClick={() => handleClick(country)}>Show</button> </li>)} </ul>
+      
+      if (props.isShown === true){
+        singleCountryInfo = showCountryData(clickedCountry)
       }
 
-      return countryInfo
+    } else if (countriesLength === 1){
+      singleCountryInfo = showCountryData(searchedCountries[0])
+      
     }
+    
+    return [countryInfo, singleCountryInfo]
+  }
 
-    return (
+  function showCountryData(country){
+
+    const singleCountryInfo = <div>
+        <h1>{country.name.common}</h1>
+        <div>
+          <p>Capital: {country.capital}</p>
+          <p>Area: {country.area}</p>
+        </div>
+        <div>
+          <strong>Languages:</strong>
+          <ul>
+            {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+          </ul>
+        </div>
+        <div>
+          <img src={country.flags.png} alt='country flag'/>
+        </div>
+      </div>
+      
+    return singleCountryInfo
+  }
+
+  return (
       <div>
         {countryData}
+        {singleCountryData}
       </div>
     )
 
-  
-  
-  
-  
 }
 
 function App() {
