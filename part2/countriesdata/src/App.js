23c54756e11e94dccb779885d0c6468fc687b5b9
Emilie Sing <sing.emilie.es@gmail.com>
2022-11-22 10:23:17 -0500

    Refactoring countryData for exercise 2.14

--- part2/countriesdata/src/App.js
+++ part2/countriesdata/src/App.js
@@ -18,36 +18,34 @@ const Search = ({countries}) => {
  return (
     <div> 
       <div>find countries: <input value={search} onChange={handleSearchCountries}/></div>
-      <CountriesList countries={searchedCountries} isShown={isShown} setIsShown={setIsShown}/>
+      <Countries countries={searchedCountries} isShown={isShown} setIsShown={setIsShown}/>
     </div>
   )
 }
 
-const CountriesList = (props) => {
+const Countries = (props) => {
   const countries = props.countries
 
   const [clickedCountry, setClickedCountry] = useState()
 
-  const [countryData, singleCountryData] = CountriesDisplay(countries)
-
-  function handleClick(country){
-
-    props.setIsShown(true)
-    setClickedCountry(country)
-  }
+  CountriesDisplay(countries)
 
   function CountriesDisplay(searchedCountries) {
     
     const countriesLength = searchedCountries.length
 
-    let countryInfo;
-    let singleCountryInfo;
+    console.log(countriesLength)
 
     if (countriesLength > 10){
+      console.log('Over 10')
       
-      countryInfo = <div>Too many matches, specify another filter</div>
+      return <UndefinedList />
       
-    } else if (countriesLength <= 10 && countriesLength > 1){
+    } 
+    
+    if (countriesLength <= 10 && countriesLength > 1){
+
+      console.log('Between 1 and 10')
 
       const sortedCountriesList = countries.sort(function (a, b) {
         if (a.name.common < b.name.common){
@@ -59,23 +57,68 @@ const CountriesList = (props) => {
         }
       })
 
-      countryInfo = <ul> {sortedCountriesList.map(country => <li key={country.name.common}>{country.name.common} <button onClick={() => handleClick(country)}>Show</button> </li>)} </ul>
-      
-      if (props.isShown === true){
-        singleCountryInfo = showCountryData(clickedCountry)
-      }
+      return <CountriesList countries={sortedCountriesList} handleShown={props.setIsShown} handleClickedCountry={setClickedCountry}/>
 
-    } else if (countriesLength === 1){
-      singleCountryInfo = showCountryData(searchedCountries[0])
-      
-    }
+    } 
     
-    return [countryInfo, singleCountryInfo]
+    if (countriesLength === 1){
+      console.log('one country')
+
+      return <SingleCountry countryData={searchedCountries[0]} />
+     
+    }
+
+    if (props.isShown === true){
+
+      return <SingleCountry countryData={clickedCountry} />
+    }
+  }
+}
+
+function UndefinedList () {
+  return (
+    <div>Too many matches, specify another filter</div>
+  )
   }
 
-  function showCountryData(country){
+const CountriesList = (props) => {
+  const countries = props.countries
 
-    const singleCountryInfo = <div>
+  function handleClick(country){
+
+    props.handleShown(true)
+    props.handleClickedCountry(country)
+  }
+
+  return (
+    <ul> 
+      {countries.map(country => 
+        <li key={country.name.common}>{country.name.common} 
+          <Button handleClick={() => handleClick()} />
+        </li>)} 
+      </ul>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>Show</button> 
+  )
+}
+
+const SingleCountry = ({country}) => {
+  
+  return(
+    <div>
+      <CountryData country={country} />
+      <Weather country={country} />
+    </div>
+  )
+}
+
+const CountryData = ({country}) => {
+  return (
+    <div>
         <h1>{country.name.common}</h1>
         <div>
           <p>Capital: {country.capital}</p>
@@ -91,17 +134,28 @@ const CountriesList = (props) => {
           <img src={country.flags.png} alt='country flag'/>
         </div>
       </div>
-      
-    return singleCountryInfo
-  }
+  )
+}
 
-  return (
-      <div>
-        {countryData}
-        {singleCountryData}
-      </div>
-    )
+const Weather = ({country}) => {
+  const [weather, setWeather] = useState([])
+  const api_key = process.env.REACT_APP_API_KEY
+  const lat = country.capitalInfo.latlng[0]
+  const lng = country.capitalInfo.latlng[1]
 
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${api_key}`)
+      .then(response => {
+        setWeather(response.data)
+      })
+  }, [])
+
+  return(
+    <div>
+      {weather.temperature}
+    </div>
+  )
 }
 
 function App() {
