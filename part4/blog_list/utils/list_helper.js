0c9973abe8fa3cbf02c4264ae8cc9d94265bf42f
Emilie Sing <sing.emilie.es@gmail.com>
2022-12-07 15:18:06 -0500

    Completed 4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -5,13 +5,38 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
+    console.log(item)
     return sum + item.likes
   }
 
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (favorite, item) => {
+    console.log(item.author)
+    if (favorite.likes > item.likes) {
+      return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+      }
+    } else {
+      return {
+        title: item.title,
+        author: item.author,
+        likes: item.likes
+      }
+    }
+  }
+
+  return blogs.length === 0
+    ? {}
+    : blogs.reduce(reducer, blogs[0])
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
