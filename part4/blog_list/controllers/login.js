33ea9f15a8a73fa18d42e190d471cd551cdb3c8a
Emilie Sing <sing.emilie.es@gmail.com>
2022-12-13 15:54:36 -0500

    Completed 4.18

---
+++ part4/blog_list/controllers/login.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const username = body.username
+  const password = body.password
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id
+  }
+
+  const token = jwt.sign(
+    userForToken, 
+    process.env.SECRET,
+    {expiresIn: 60*60}
+  )
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name})
+
+})
+
+module.exports = loginRouter
