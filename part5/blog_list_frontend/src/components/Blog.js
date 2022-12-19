a36143f64ab1655250460d8a1bf4a41a8557aa04
Emilie Sing <sing.emilie.es@gmail.com>
2022-12-19 16:16:49 -0500

    Completed 5.7

--- part5/blog_list_frontend/src/components/Blog.js
+++ part5/blog_list_frontend/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+
+  const [visible, setVisible] = useState('false')
+  const showWhenVisible = { 
+    display: visible ? 'none' : '' ,
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const hideWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>View</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>Hide</button>
+        <p>{blog.url}</p>
+        likes {blog.likes} <button>Like</button>
+        <p>{blog.author}</p>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
