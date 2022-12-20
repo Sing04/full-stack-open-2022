f3298076a39a0aa8b880f4a763d586a338c56f7a
Emilie Sing <sing.emilie.es@gmail.com>
2022-12-20 09:33:12 -0500

    Completed 5.9

--- part5/blog_list_frontend/src/App.js
+++ part5/blog_list_frontend/src/App.js
@@ -15,9 +15,20 @@ const App = () => {
     color: 'white'
   })
 
+  const sortBlogs = (blog1, blog2) => {
+    if (blog1.likes < blog2.likes) {
+      return 1
+    } else if (blog1.likes > blog2.likes) {
+      return -1
+    } else {
+      return 0
+    }
+  }
+
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+    blogService.getAll()
+    .then(blogs =>
+      setBlogs(blogs.sort(sortBlogs))
     )  
   }, [])
 
