004c8ced9d39e53d2b0e0dd0fb55fa8ae711801f
Emilie Sing <sing.emilie.es@gmail.com>
2023-01-13 16:46:57 -0500

    Completed 7.15

--- part5/blog_list_frontend/src/App.js
+++ part5/blog_list_frontend/src/App.js
@@ -3,10 +3,12 @@ import BlogList from './components/BlogList'
 import Login from './components/Login'
 import Notification from './components/Notification'
 import Users from './components/Users'
+import User from './components/User'
 import { useDispatch, useSelector } from 'react-redux'
 import { setUser } from './reducers/loginUserReducer'
 import { Link, Routes, Route } from 'react-router-dom'
 import Logout from './components/Logout'
+import { useMatch } from 'react-router-dom'
 
 const App = () => {
   const dispatch = useDispatch()
@@ -19,7 +21,14 @@ const App = () => {
     }
   }, [])
 
-  const user = useSelector(state => state.loginUser)
+  const users = useSelector(state => state.users)
+
+  const match = useMatch('/users/:id')
+  const user = match
+    ? users.find(user => user.id === match.params.id)
+    : null
+
+  const loginUser = useSelector(state => state.loginUser)
 
   const padding = {
     paddingRight: 8
@@ -28,11 +37,11 @@ const App = () => {
   return (
     <div className='container'>
       <div>
-        {user &&
+        {loginUser &&
           <div>
             <Link style={padding} to='/'>Blogs</Link>
             <Link style={padding} to='/users'>Users</Link>
-            <em style={{ fontStyle: 'italic' }}>{user.name} logged in </em> <Logout />
+            <em style={{ fontStyle: 'italic' }}>{loginUser.name} logged in </em> <Logout />
           </div>
         }
       </div>
@@ -40,6 +49,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<BlogList />} />
         <Route path='/users' element={<Users /> } />
+        <Route path='/users/:id' element={ <User user={user} />} />
         <Route path='/login' element={<Login />} />
       </Routes>
     </div>
--- part5/blog_list_frontend/src/components/User.js
+++ part5/blog_list_frontend/src/components/User.js
@@ -1,12 +1,25 @@
-const User = () => {
-  const user = ''
-  if (!user) {
+const User = ({ user }) => {
+
+  const header = {
+    marginTop: 30,
+    marginBottom: 15
+  }
+
+  if(!user) {
     return null
   }
 
   return (
     <div>
-      <h2>{user.name}</h2>
+      <h2 style={header}>{user.name}</h2>
+      <h5 style={header}>Added Blogs</h5>
+      <ul>
+        {user.blogs.map(blog =>
+          <li key={blog.title}>{blog.title}</li>
+        )}
+      </ul>
     </div>
   )
 }
+
+export default User
